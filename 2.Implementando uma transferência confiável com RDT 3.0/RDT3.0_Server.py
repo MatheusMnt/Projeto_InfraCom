6dc6c5e937449bcc0e5975939755efca3366838e
MatheusMnt <matheus.augustomnt@gmail.com>
2025-03-16 13:19:50 -0300

    fix(): state0 e state1 no servidor

--- 2.Implementando uma transferência confiável com RDT 3.0/RDT3.0_Server.py
+++ 2.Implementando uma transferência confiável com RDT 3.0/RDT3.0_Server.py
@@ -16,11 +16,40 @@ server_socket.bind((HOST, PORT))
 print(f"Servidor UDP aguardando conexões na porta {PORT}...")
 
 def rdt_rcv():
+    global num_seq, rcv_base, send_base, client_address
     data, client_address = server_socket.recvfrom(BUFFER_SIZE)
     msg = data.decode()
     print(f"Recebendo arquivo '{msg}' de {client_address}")
-    return client_address, msg, msg[:2] #num_seq
-
+    return msg
 
 def rdt_snd(msg, client_address):
       server_socket.sendto(msg.encode(), client_address)  
+
+def state0():
+    global num_seq, send_base, rcv_base, client_address
+    rcv_msg = rdt_rcv() 
+    # se a mensagem não é a esperada, reenvia o ACK
+    if rcv_msg[:2] != rcv_base:
+       ACK0 = str(num_seq).zfill(2) + str(rcv_base).zfill(2)
+       rdt_snd(ACK0, client_address)
+    else:
+      #TODO implementar o deliver(data)
+      rcv_base+=1
+      num_seq+=1
+      ACK1 = str(num_seq).zfill(2) + str(rcv_base).zfill(2)
+      rdt_snd(ACK1, client_address)
+
+def state1():
+    global num_seq, send_base, rcv_base, client_address
+    rcv_msg = rdt_rcv()
+    # se a mensagem não é a esperada, reenvia o ACK
+    if rcv_msg[:2] != rcv_base:
+         ACK1 = str(num_seq).zfill(2) + str(rcv_base).zfill(2)
+         rdt_snd(ACK1, client_address)
+    else:
+      #TODO implementar o deliver(data)
+      rcv_base-=1
+      num_seq-=1
+      ACK0 = str(num_seq).zfill(2) + str(rcv_base).zfill(2)
+      rdt_snd(ACK0, client_address)
+
